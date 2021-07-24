9d74fbb7b71a5cbd6e296e9e422449dffabe4ddc
oskarbukovsky <janoskarbukovsky@gmail.com>
2021-07-25 00:00:59 +0200

    Update sw.js

--- files/js/sw.js
+++ files/js/sw.js
@@ -1,24 +1,3 @@
-/*
-Copyright 2015, 2019 Google Inc. All Rights Reserved.
- Licensed under the Apache License, Version 2.0 (the "License");
- you may not use this file except in compliance with the License.
- You may obtain a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS,
- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- See the License for the specific language governing permissions and
- limitations under the License.
-*/
-
-// Incrementing OFFLINE_VERSION will kick off the install event and force
-// previously cached resources to be updated from the network.
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-// Customize this with a different URL if needed.
-const OFFLINE_URL = 'offline.html';
-
-
 self.addEventListener('install', function (event) {
   console.log('SW Installed');
   event.waitUntil(
@@ -34,21 +13,9 @@ self.addEventListener('install', function (event) {
   );
 });
 
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
 });
-
-
-
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
@@ -62,9 +29,3 @@ self.addEventListener('fetch', function(event) {
       })
   );
 });
-
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
