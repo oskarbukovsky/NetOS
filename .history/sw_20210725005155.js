57874e30089923f28ca701fa307e1e2f8802362b
oskarbukovsky <janoskarbukovsky@gmail.com>
2021-07-25 01:11:52 +0200

    update

---
+++ .history/sw_20210725005155.js
@@ -0,0 +1,32 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '.',
+          'https://winaero.com/blog/wp-content/uploads/2019/06/WIndows-Terminal-icon.png'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
