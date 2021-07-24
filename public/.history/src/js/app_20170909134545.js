38030efd996a6c5c571b111bfbb59d819c781b8c
oskarbukovsky <janoskarbukovsky@gmail.com>
2021-07-24 20:53:09 +0200

    fix

--- public/.history/src/js/app_20170909134545.js
+++ public/.history/src/js/app_20170909134545.js
@@ -4,7 +4,7 @@ var pwaCardDetails = pwaCard.querySelector('.card__details');
 var detailsShown = false;
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/NetOS/public/sw.js')
+  navigator.serviceWorker.register('/sw.js')
     .then(function() {
       console.log('SW registered');
     });
