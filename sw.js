7856220f6142c99f85ca54b6eaef3d3c3102041a
oskarbukovsky <janoskarbukovsky@gmail.com>
2021-07-25 02:01:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -18,8 +18,16 @@ self.addEventListener('activate', function () {
 self.addEventListener('fetch', function(event) {
   console.log("SW Fetch");
   event.respondWith(
-    fetch(event.request).catch(function() {
-      return caches.match(event.request);
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // Fall back to network
+      return response || fetch(event.request);
+    }).catch(function() {
+      // If both fail, show a generic fallback:
+      return caches.match('/offline.html');
+      // However, in reality you'd have many different
+      // fallbacks, depending on URL & headers.
+      // Eg, a fallback silhouette image for avatars.
     })
   );
 });
